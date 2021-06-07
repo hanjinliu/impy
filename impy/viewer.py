46d2c97024992bec509a3bc93e13eb89bb65b645
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2021-06-07 14:39:26 +0900

    delayed key binding for fast startup

--- impy/viewer.py
+++ impy/viewer.py
@@ -13,8 +13,6 @@ magicgui = ImportOnRequest("magicgui")
 
 # import napari.viewer
 # TODO: 
-# - Start different window if added object is apparently different. To do this, self.viewer should be
-#   a property that returns the most recent window.
 # - Layer does not remember the original data after c-split.
 # - 
     
@@ -85,12 +83,12 @@ class napariWindow:
             raise TypeError("`key` must be str.")
         if key in self._viewers.keys():
             raise ValueError(f"Key {key} already exists.")
+        if not self._viewers:
+            bind_keys()
         viewer = napari.Viewer(title=key)
         default_viewer_settings(viewer)
         # Add dock widgets
         viewer.window.add_dock_widget(self._make_dock_window(), area="left")
-        # Add key bindings
-        viewer.bind_key("Control-Shift-x")(lambda v: self.crop(dims="tzc", viewer=v))
         
         self._viewers[key] = viewer
         self._front_viewer = key
@@ -161,10 +159,6 @@ class napariWindow:
         mf = MarkerFrame(data, columns = self.axes)
         mf.set_scale(self)
         return mf
-    
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-    #    Key Bindings
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
     
     def crop(self, dims:str="tzc", viewer:napari.Viewer=None):
         """
@@ -583,79 +577,126 @@ def front_image(viewer):
     return front
 
 # key binding
+def bind_keys():
+    @napari.Viewer.bind_key("Control-Shift-a")
+    def hide_others(viewer):
+        """
+        Make selected layers visible and others invisible. If key is pushed for a long time, then the visibility
+        is restored upon release
 
-@napari.Viewer.bind_key("Control-Shift-a")
-def hide_others(viewer):
-    """
-    Make selected layers visible and others invisible. If key is pushed for a long time, then the visibility
-    is restored upon release
-
-    Parameters
-    ----------
-    viewer : napari.Viewer, optional
-        Target viewer.
-    """
-    visibility = []
-    selected = viewer.layers.selection
-    for layer in viewer.layers:
-        visibility.append(layer.visible)
-        if layer in selected:
-            layer.visible = True
-        else:
-            layer.visible = False
-    
-
-@napari.Viewer.bind_key("Control-h")
-def hello(viewer):
-    viewer.status = "Hello world"
-    yield
-    viewer.status = "goodbye world"
-
-@napari.Viewer.bind_key("Alt-l")
-def shapes_to_labels(viewer):
-    """
-    Convert manually drawn shapes to labels and store in `destination`.
-
-    Parameters
-    ----------
-    destination : (list of) LabeledArray, optional
-        To which labels will be stored, by default None
-    viewer : napari.Viewer, optional
-        Target viewer.
+        Parameters
+        ----------
+        viewer : napari.Viewer, optional
+            Target viewer.
+        """
+        visibility = []
+        selected = viewer.layers.selection
+        for layer in viewer.layers:
+            visibility.append(layer.visible)
+            if layer in selected:
+                layer.visible = True
+            else:
+                layer.visible = False
         
-    Returns
-    -------
-    Label
-        Last appended label.
-    """        
-    
-    # determine
-    destinations = [l.data for l in _iter_selected_layer(viewer, "image")]
-    if len(destinations) == 0:
-        destinations = [front_image(viewer).data]
-    
-    for dst in destinations:
-        # check zoom_factors
-        d = viewer.dims
-        scale = {a: r[2] for a, r in zip(d.axis_labels, d.range)}
-        zoom_factors = [scale[a]/dst.scale[a] for a in "yx"]
-        if np.unique(zoom_factors).size == 1:
-            zoom_factor = zoom_factors[0]
-        else:
-            raise ValueError("Scale mismatch in images and napari world.")
-        # make labels from selected shapes
-        shapes = [to_labels(layer, dst.shape, zoom_factor=zoom_factor) 
-                    for layer in _iter_selected_layer(viewer, "shape")]
-        # append labels to each destination
-        label = sum(shapes)
-        if hasattr(dst, "labels"):
-            print(f"Label already exist in {dst}. Overlapped.")
-            del dst.labels
-        dst.append_label(label)
+
+    @napari.Viewer.bind_key("Control-h")
+    def hello(viewer):
+        viewer.status = "Hello world"
+        yield
+        viewer.status = "goodbye world"
+
+    @napari.Viewer.bind_key("Alt-l")
+    def shapes_to_labels(viewer):
+        """
+        Convert manually drawn shapes to labels and store in `destination`.
+
+        Parameters
+        ----------
+        destination : (list of) LabeledArray, optional
+            To which labels will be stored, by default None
+        viewer : napari.Viewer, optional
+            Target viewer.
+            
+        Returns
+        -------
+        Label
+            Last appended label.
+        """        
         
-    return dst.labels
+        # determine
+        destinations = [l.data for l in _iter_selected_layer(viewer, "image")]
+        if len(destinations) == 0:
+            destinations = [front_image(viewer).data]
+        
+        for dst in destinations:
+            # check zoom_factors
+            d = viewer.dims
+            scale = {a: r[2] for a, r in zip(d.axis_labels, d.range)}
+            zoom_factors = [scale[a]/dst.scale[a] for a in "yx"]
+            if np.unique(zoom_factors).size == 1:
+                zoom_factor = zoom_factors[0]
+            else:
+                raise ValueError("Scale mismatch in images and napari world.")
+            # make labels from selected shapes
+            shapes = [to_labels(layer, dst.shape, zoom_factor=zoom_factor) 
+                        for layer in _iter_selected_layer(viewer, "shape")]
+            # append labels to each destination
+            label = sum(shapes)
+            if hasattr(dst, "labels"):
+                print(f"Label already exist in {dst}. Overlapped.")
+                del dst.labels
+            dst.append_label(label)
+            
+        return dst.labels
 
+    @napari.Viewer.bind_key("Control-Shift-x")
+    def crop(viewer):
+        """
+        Crop images at the edges of the napari viewer. This function can be called with key binding by
+        default.
 
+        Parameters
+        ----------
+        dims : str, optional
+            Which axes will not be cropped. Generally when an image stack is cropped at the edges, all 
+            the images along t-axis should be cropped at the same edges. On the other hand, images at
+            different positions (different p-coordinates) should not. That is why default is "tzc".
+        viewer : napari.Viewer, optional
+            Target viewer.
+        """        
+        
+        imglist = list(filter(lambda x: isinstance(x, napari.layers.Image), viewer.layers.selection))
+        count = 0
+        for layer in imglist:
+            sl = []
+            translate = []
+            for i, (start, end) in enumerate(layer.corner_pixels.T):
+                start, end = int(start), int(end+1)
+                if start+1 < end:
+                    if layer.data.axes[i] in "yx":
+                        translate.append(start*layer.data.scale[layer.data.axes[i]])
+                    sl.append(slice(start, end))
+                else:
+                    if layer.data.axes[i] in "tzc":
+                        sl.append(slice(None))
+                    else:
+                        sl.append(start)
+            
+            img = layer.data[tuple(sl)]
+            if img.size > 0:
+                kwargs = dict(name=img.name+"-crop", translate=translate)
+                if isinstance(img, PhaseArray):
+                    kwargs["colormap"] = "hsv"
+                    kwargs["contrast_limits"] = img.border
+                
+                scale = make_world_scale(img)
+                            
+                viewer.add_image(img, scale=scale, **kwargs)
+                count += 1
+        
+        if count == 0:
+            viewer.status = "No image was cropped"
+        return None
 
 window = napariWindow()
 
