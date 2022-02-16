0142b3ce17642dedab3ac88f5754af5018bb25dc
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2022-02-16 17:35:51 +0900

    add test

---
+++ tests/test_shape.py
@@ -0,0 +1,17 @@
+import impy as ip
+
+def test_shapes():
+    shape = (4, 5, 6)
+    img = ip.zeros(shape, axes="zyx")
+    assert img.shape == shape
+    assert img.shape.z == shape[0]
+    assert img.shape.y == shape[1]
+    assert img.shape.x == shape[2]
+    
+    shape = (9, 3, 12)
+    img = ip.zeros(shape, axes="zyx")
+    assert img.shape == shape
+    assert img.shape.z == shape[0]
+    assert img.shape.y == shape[1]
+    assert img.shape.x == shape[2]
+    
