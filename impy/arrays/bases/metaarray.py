broadcasting bug fix

impy/arrays/bases/metaarray.py
@@ -442,19 +442,16 @@ class MetaArray(AxesMixin, np.ndarray):
         out._set_info(self, new_axes=new_axes)
         return out
     
-    def _broadcast(self, value):
+    def _broadcast(self, value: Any):
         """
         More flexible broadcasting. If `self` has "zcyx"-axes and `value` has "zyx"-axes, then
         they should be broadcasted by stacking `value` along "c"-axes
-        """        
-        if isinstance(value, MetaArray):
+        """
+        if not isinstance(value, np.ndarray) or self.ndim == value.ndim:
+            return value
+        
+        if isinstance(value, MetaArray) and value.axes != self.axes[:value.ndim]:
             value = axesop.add_axes(self.axes, self.shape, value, value.axes)
-        elif isinstance(value, np.ndarray):
-            try:
-                if self.sizesof("yx") == value.shape:
-                    value = axesop.add_axes(self.axes, self.shape, value)
-            except AttributeError:
-                pass
         return value
     
     def __add__(self, value) -> Self:

impy/utils/axesop.py
@@ -47,12 +47,12 @@ def complement_axes(axes, all_axes="ptzcyx"):
 
 def switch_slice(axes, all_axes, ifin=np.newaxis, ifnot=":"):
     if ifnot == ":":
-        ifnot = [slice(None)]*len(all_axes)
+        ifnot = [slice(None)] * len(all_axes)
     elif not hasattr(ifnot, "__iter__"):
-        ifnot = [ifnot]*len(all_axes)
+        ifnot = [ifnot] * len(all_axes)
         
     if not hasattr(ifin, "__iter__"):
-        ifin = [ifin]*len(all_axes)
+        ifin = [ifin] * len(all_axes)
         
     sl = []
     for a, slin, slout in zip(all_axes, ifin, ifnot):
@@ -71,7 +71,7 @@ def slice_axes(axes, key):
         if key.ndim == 1:
             new_axes = axes
         else:
-            new_axes = None
+            new_axes = [UndefAxis()] + axes[key.ndim:]
         return new_axes
     else:
         _keys = (key,)