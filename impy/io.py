32634777d9c276578e41a5ae421d57cb4e2e90cf
Hanjin Liu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2023-02-23 20:51:40 +0900

    support nd2

--- impy/io.py
+++ impy/io.py
@@ -552,6 +552,40 @@ def _(path: str, img: ImpyArray, lazy: bool = False):
         )
     return None
 
+@IO.mark_reader(".nd2")
+def _(path: str, memmap: bool = False):
+    import nd2
+    from dataclasses import is_dataclass, asdict
+
+    with nd2.ND2File(path) as f:
+        if not memmap:
+            image = f.asarray()
+        else:
+            image = f.to_dask()
+        axes = "".join(f.sizes.keys()).lower()
+        vsize = f.voxel_size()
+        scale = dict(z=vsize.z, y=vsize.y, x=vsize.x)
+        metadata = f.metadata
+        labels = None
+        if "c" in axes:
+            try:
+                labels = []
+                for chn in metadata.channels:
+                    labels.append(chn.channel.name)
+            except Exception:
+                pass
+        if is_dataclass(metadata):
+            metadata = asdict(metadata)
+        
+    return ImageData(
+        image=image,
+        axes=axes,
+        scale=scale,
+        unit="µm",
+        metadata=metadata,
+        labels=labels,
+    )
+
 @IO.mark_writer("")
 def _(path: str, img: ImpyArray, lazy: bool = False):
     raise ValueError(f"Input path {path!r} does not have file extension.")
